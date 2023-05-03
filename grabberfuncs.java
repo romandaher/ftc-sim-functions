bd4a8652197e50c1e6d10c65b858d676ab70c2d3
Roman Daher <84356412+romandaher@users.noreply.github.com>
2023-05-03 11:57:16 -0400

    Update grabberfuncs.java

--- grabberfuncs.java
+++ grabberfuncs.java
@@ -23,7 +23,6 @@ public void rotate(int deg) {
     R(0);
     sleep(100);
 }
-
 public void stop(){
     R(0);
     L(0);
@@ -34,16 +33,10 @@ public void upd(){
     telemetry.addData("Green", color1.green());
     telemetry.update();
 }
-
-
 public void grab(int pos) {
     grabber.setPosition(pos);
     sleep(500);
 }
-
-
-
-
 public void back(int ms) {
     L(1);
     R(-1);
@@ -51,11 +44,6 @@ public void back(int ms) {
     stop();
     sleep(200);
 }
-
-
-
-
-
 public void go_wall() {
     while (distance1.getDistance(DistanceUnit.CM) > 80) {
       go(1);
